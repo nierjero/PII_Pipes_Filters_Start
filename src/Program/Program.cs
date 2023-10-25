dd76e2a911f9b16b720f2c1365e5bb83b03920d2
nierjero <jeronimo.gonzalez@correo.ucu.edu.uy>
2023-10-25 20:57:16 -0300

    ej3

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -20,23 +20,36 @@ namespace CompAndDel
             IPipe pipe2 = new PipeSerial(filter2, pipe3);
             IPipe pipe1 = new PipeSerial(filter1, pipe2);
 
-            // Envia la imagen a través de los Pipes y guarda las imágenes intermedias
+            // Envia la imagen a través de los Pipes y guarda las imágenes 
             picture = pipe1.Send(picture);
 
-            // Guarda la imagen intermedia después de FilterGreyscale
+            // Guarda la imagen  después de FilterGreyscale
             provider.SavePicture(picture, @"C:\Users\Jero\Desktop\ProgII\ReposPipe\PII_Pipes_Filters_Start\src\Program\intermediate1.jpg");
 
-            // Continua con la secuencia
+            // Continua por los pipes
             picture = pipe2.Send(picture);
 
             // Guarda la imagen intermedia después de FilterNegative
             provider.SavePicture(picture, @"C:\Users\Jero\Desktop\ProgII\ReposPipe\PII_Pipes_Filters_Start\src\Program\intermediate2.jpg");
 
-            // Continua con la secuencia
+            // Continua por los pipes
             picture = pipe3.Send(picture);
 
             // Guarda la imagen final
             provider.SavePicture(picture, @"C:\Users\Jero\Desktop\ProgII\ReposPipe\PII_Pipes_Filters_Start\src\Program\final.jpg");
+
+
+            var twitterImage = new TwitterImage();
+            string intermediateImagePath1 = @"C:\Users\Jero\Desktop\ProgII\ReposPipe\PII_Pipes_Filters_Start\src\Program\intermediate1.jpg";
+            string intermediateImagePath2 = @"C:\Users\Jero\Desktop\ProgII\ReposPipe\PII_Pipes_Filters_Start\src\Program\intermediate2.jpg";
+
+            // Publica primera imagen 
+            twitterImage.PublishToTwitter("Transformación intermedia 1", intermediateImagePath1);
+            // Publica segunda imagen 
+            twitterImage.PublishToTwitter("Transformación intermedia 2", intermediateImagePath2);
+            
+
+
         }
     }
 }
