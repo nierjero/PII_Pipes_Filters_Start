b4b9eed9d623e7df1075fbe3c54740026e2ef078
nierjero <jeronimo.gonzalez@correo.ucu.edu.uy>
2023-10-25 20:43:11 -0300

    hasta ej2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,36 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"C:\Users\Jero\Desktop\ProgII\ReposPipe\PII_Pipes_Filters_Start\src\Program\luke.jpg");
+
+            // Crea los objetos Filter en orden inverso
+            IFilter filter2 = new FilterNegative();
+            IFilter filter1 = new FilterGreyscale();
+
+            // Crea los objetos Pipe en orden inverso
+            IPipe pipe3 = new PipeNull();
+            IPipe pipe2 = new PipeSerial(filter2, pipe3);
+            IPipe pipe1 = new PipeSerial(filter1, pipe2);
+
+            // Envia la imagen a través de los Pipes y guarda las imágenes intermedias
+            picture = pipe1.Send(picture);
+
+            // Guardar la imagen intermedia después de FilterGreyscale
+            provider.SavePicture(picture, @"C:\Users\Jero\Desktop\ProgII\ReposPipe\PII_Pipes_Filters_Start\src\Program\intermediate1.jpg");
+
+            // Continuar con la secuencia
+            picture = pipe2.Send(picture);
+
+            // Guardar la imagen intermedia después de FilterNegative
+            provider.SavePicture(picture, @"C:\Users\Jero\Desktop\ProgII\ReposPipe\PII_Pipes_Filters_Start\src\Program\intermediate2.jpg");
+
+            // Continuar con la secuencia
+            picture = pipe3.Send(picture);
+
+            // Guardar la imagen final
+            provider.SavePicture(picture, @"C:\Users\Jero\Desktop\ProgII\ReposPipe\PII_Pipes_Filters_Start\src\Program\final.jpg");
         }
     }
 }
+
